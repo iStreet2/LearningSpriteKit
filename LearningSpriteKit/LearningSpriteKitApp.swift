d7e9bb52cdd82349753cb5c9d552eebf948e5cc7
Gabriel Vicentin Negro <gabrielvicentinnegro@hotmail.com>
2024-01-15 14:13:38 -0300

    Initial Commit

---
+++ LearningSpriteKit/LearningSpriteKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningSpriteKitApp.swift
+//  LearningSpriteKit
+//
+//  Created by Gabriel Vicentin Negro on 15/01/24.
+//
+
+import SwiftUI
+
+@main
+struct LearningSpriteKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
